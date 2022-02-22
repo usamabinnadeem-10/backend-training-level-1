a90baa1a1496b768cccfd088f939169f2b6ce8a2
Usama Bin Nadeem <usamabinnadeem10@gmail.com>
2022-02-22 09:59:56 +0500

    prime number function

--- functions.py
+++ functions.py
@@ -1,6 +1,3 @@
-from math import ceil
-
-
 def fibonacci(num_of_terms=1):
     output = []
     for idx in range(num_of_terms):
@@ -10,8 +7,17 @@ def fibonacci(num_of_terms=1):
             output.append(output[idx - 1] + output[idx - 2])
     return output
 
-
 def prime_numbers(limit):
-    pass
+    primes = []
+    filter = [True] * (limit + 1)
+    for i in range(2, limit+1):
+        if filter[i]:
+            primes.append(i)
+            # now set all the multiples of i to False in filter because they are not primes
+            for f in range(i, limit+1, i):
+                filter[f] = False
+        
+    return primes
+
     
         
